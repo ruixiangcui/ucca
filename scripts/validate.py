f5140984efc5a2e0daca85d88736b6543d662604
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-10-18 13:58:57 +0300

    Check for multigraph in validation

--- scripts/validate.py
+++ scripts/validate.py
@@ -9,23 +9,25 @@ from ucca.validation import validate
 
 
 class Validator:
-    def __init__(self, normalization=False, extra=False, linkage=True, strict=False):
+    def __init__(self, normalization=False, extra=False, linkage=True, multigraph=False, strict=False):
         self.normalization = normalization
         self.extra = extra
         self.linkage = linkage
+        self.multigraph = multigraph
         self.strict = strict
 
     def validate_passage(self, passage):
         if self.normalization:
             normalize(passage, extra=self.extra)
-        errors = list(validate(passage, linkage=self.linkage))
+        errors = list(validate(passage, linkage=self.linkage, multigraph=self.multigraph))
         if self.strict:
             print_errors(passage.ID, errors)
         return passage.ID, errors
 
 
 def main(args):
-    validator = Validator(args.normalize, args.extra, linkage=args.linkage, strict=args.strict)
+    validator = Validator(args.normalize, args.extra, linkage=args.linkage, multigraph=args.multigraph,
+                          strict=args.strict)
     with Pool(10) as pool:
         errors = pool.map(validator.validate_passage,
                           get_passages_with_progress_bar(args.filenames, desc="Validating", converters={}))
@@ -59,4 +61,5 @@ if __name__ == "__main__":
     argparser.add_argument("-n", "--normalize", action="store_true", help="normalize before validation")
     argparser.add_argument("-e", "--extra", action="store_true", help="extra normalization rules")
     argparser.add_argument("--no-linkage", dest="linkage", action="store_false", help="skip linkage validations")
+    argparser.add_argument("--multigraph", action="store_true", help="allow multiple edges with the same parent+child")
     main(check_args(argparser, argparser.parse_args()))
