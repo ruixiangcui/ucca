ef47fd8af0ce4401239fc158e3072091d0239520
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-12-18 14:38:16 +0200

    Write created task IDs to log file; add progress bar

--- uccaapp/create_tokenization_tasks.py
+++ uccaapp/create_tokenization_tasks.py
@@ -3,48 +3,34 @@ import sys
 
 import argparse
 
-from uccaapp.api import ServerAccessor
+from uccaapp.create_annotation_tasks import ServerAccessor, AnnotationTaskCreator
 
 desc = """Upload a list of tokenization tasks to a project"""
 
 
-class TokenizationTaskCreator(ServerAccessor):
+class TokenizationTaskCreator(AnnotationTaskCreator):
     def __init__(self, project_id, **kwargs):
         super().__init__(**kwargs)
         self.set_project(project_id)
 
-    def create_tokenization_task(self, filename, **kwargs):
+    def build_task(self, user_id, passage_id, **kwargs):
         del kwargs
-        with open(filename, encoding="utf-8") as f:
-            num = 0
-            for line in f:
-                line = line.strip()
-                fields = line.split()
-                if len(fields) != 2:
-                    print("Error in line: '%s'" % line, file=sys.stderr)
-                    continue
-                user_id, passage_id = fields
-                user = self.get_user(user_id)
-                passage = self.get_passage(passage_id)
-                task_in = dict(type="TOKENIZATION", status="NOT_STARTED", project=self.project,
-                               user=user, passage=passage,
-                               manager_comment="passage #%s" % passage["id"],
-                               user_comment="", parent=None,
-                               is_demo=False, is_active=True)
-                tok_task_out = self.create_task(**task_in)
-                print("Task #%s uploaded." % tok_task_out["id"], file=sys.stderr)
-                num += 1
-            print("Uploaded %d tasks successfully." % num, file=sys.stderr)
+        user = self.get_user(user_id)
+        passage = self.get_passage(passage_id)
+        return dict(type="TOKENIZATION", project=self.project, user=user, passage=passage,
+                    manager_comment="passage #%s" % passage["id"], user_comment="", parent=None, is_demo=False,
+                    is_active=True)
 
     @staticmethod
     def add_arguments(argparser):
         argparser.add_argument("filename", help="a file where each line is a <User ID> <Passage ID>")
+        argparser.add_argument("-l", "--log", help="filename to write log of uploaded passages to")
         ServerAccessor.add_project_id_argument(argparser)
         ServerAccessor.add_arguments(argparser)
 
 
 def main(**kwargs):
-    TokenizationTaskCreator(**kwargs).create_tokenization_task(**kwargs)
+    TokenizationTaskCreator(**kwargs).create_tasks(**kwargs)
 
 
 if __name__ == "__main__":
