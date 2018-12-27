71dd9def3570828b406073d1c567d4e229028ce9
omriabnd <omriabnd@gmail.com>
2018-12-27 10:34:35 +0200

    added output option

--- uccaapp/export_units_by_filter.py
+++ uccaapp/export_units_by_filter.py
@@ -8,39 +8,49 @@ desc = "Get all units according to a specified filter. Units that meet any of th
 
 def get_top_level_ancestor(node):
     """
-    Traverses the passage upwards until a root node is reached
+    Traverses the passage upwards until a unit which is immediately below the root is reached
     :param node:
     :return:
     """
-    parent = node
-    while parent.fparent:
-        parent = parent.fparent
-    return parent
+    # if node is already the root, return it
+    if not node.fparent:
+        return node
+    else:
+        parent = node
+        while parent.fparent.fparent:
+            parent = parent.fparent
+        return parent
 
 def isIntersecting(L1,L2):
     return set(L1) & set(L2)
 
-def main(comment = False, categories = (), **kwargs):
+def main(output = None, comment = False, categories = (), **kwargs):
     filtered_nodes = []
     for passage, task_id, user_id in TaskDownloader(**kwargs).download_tasks(**kwargs, write=False):
         for node in passage.layer(layer1.LAYER_ID).all:
             if comment and node.extra.get("remarks"):
-                filtered_nodes.append(("comment",node))
+                filtered_nodes.append(("comment",node,task_id,user_id))
             else:
                 all_tags = node.extra.get("all_tags")
                 if all_tags:
-                    intersection = set(categories) & set(node.extra.get("all_tags").split(';'))
+                    intersection = set(categories) & set(all_tags.split(';'))
                     if intersection:
-                        filtered_nodes.append((str(list(intersection)),node))
+                        filtered_nodes.append((str(list(intersection)),node,task_id,user_id))
 
-    for filter_type,node in filtered_nodes:
-        ancestor = get_top_level_ancestor(node)
-        print('\t'.join([filter_type,str(task_id),str(user_id),node.extra.get("tree_id"),node.to_text(),str(ancestor),str(node.extra.get("remarks"))]))
+    if output:
+        f = open(output,'w')
+        for filter_type,node,task_id,user_id in filtered_nodes:
+            ancestor = get_top_level_ancestor(node)
+            f.write('\t'.join([filter_type,str(task_id),str(user_id),node.extra.get("tree_id"),node.to_text(),
+                               str(ancestor),str(node.extra.get("remarks")).replace("\n","|")])+'\n')
+        f.close()
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser(description=desc)
     TaskDownloader.add_arguments(argument_parser)
-    argument_parser.add_argument("--categories", nargs="+", help="Abbreviations of the names of the categories to filter by")
+    argument_parser.add_argument("--output",
+                                 help="output file name")
+    argument_parser.add_argument("--categories", nargs="+", default=(), help="Abbreviations of the names of the categories to filter by")
     argument_parser.add_argument("--comment", action="store_true", help="Output all the units that have comments")
 
     main(**vars(argument_parser.parse_args()))
