c91611a2153b0b0ddc06d5da95ce70ba686eb14c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-12-30 13:55:55 +0200

    Add progress bar and options to normalize and validate downloaded tasks

--- uccaapp/download_task.py
+++ uccaapp/download_task.py
@@ -2,7 +2,9 @@
 import sys
 
 import argparse
+from tqdm import tqdm
 
+from ucca import normalization, validation
 from ucca.convert import from_json
 from ucca.ioutil import write_passage
 from uccaapp.api import ServerAccessor
@@ -19,18 +21,25 @@ class TaskDownloader(ServerAccessor):
             task_ids_from_file = []
             for filename in task_ids:
                 with open(filename, 'r') as f:
-                    task_ids_from_file.extend(list(filter(None, map(str.strip, f))))
+                    task_ids_from_file += list(filter(None, map(str.strip, f)))
             task_ids = task_ids_from_file
-        for task_id in task_ids:
+        for task_id in tqdm(task_ids, unit=" tasks", desc="Downloading"):
             yield self.download_task(task_id, **kwargs)
 
-    def download_task(self, task_id, write=True, binary=None, out_dir=None, prefix=None, **kwargs):
+    def download_task(self, task_id, normalize=False, write=True, validate=False, binary=None, out_dir=None,
+                      prefix=None, **kwargs):
         del kwargs
         task_json = self.get_user_task(task_id)
         user_id = task_json["user"]["id"]
         passage = from_json(task_json)
+        if normalize:
+            normalization.normalize(passage)
         if write:
             write_passage(passage, binary=binary, outdir=out_dir, prefix=prefix)
+        if validate:
+            for error in validation.validate(passage, linkage=False):
+                with tqdm.external_write_mode():
+                    print(passage.ID, task_id, user_id, task_json["user_comment"], error, sep="\t")
         return passage, task_id, user_id
 
     @staticmethod
@@ -45,7 +54,10 @@ class TaskDownloader(ServerAccessor):
     def add_write_arguments(argparser):
         argparser.add_argument("-o", "--out-dir", default=".", help="output directory")
         argparser.add_argument("-p", "--prefix", default="", help="output filename prefix")
+        argparser.add_argument("-V", "--validate", action="store_true", help="run validation on downloaded passages")
         argparser.add_argument("-b", "--binary", action="store_true", help="write in binary format (.pickle)")
+        argparser.add_argument("-n", "--no-write", action="store_false", dest="write", help="do not write files")
+        argparser.add_argument("-N", "--normalize", action="store_true", help="normalize downloaded passages")
 
 
 def main(**kwargs):
