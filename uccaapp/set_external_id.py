56952144f7dc05b83020e34e2cd6d0bd72e85c79
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-04-08 09:36:06 +0200

    Allow setting external IDs for passages given by task ID

--- uccaapp/set_external_id.py
+++ uccaapp/set_external_id.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
-import argparse
 from tqdm import tqdm
 
 from uccaapp.api import ServerAccessor
@@ -13,11 +13,14 @@ class ExternalIdSetter(ServerAccessor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def set_external_ids(self, filename, **kwargs):
+    def set_external_ids(self, filename, by_task_id=False, **kwargs):
         del kwargs
         with open(filename, encoding="utf-8") as f:
             passage_id_to_external_id = list(map(str.split, map(str.strip, f)))
         for external_id, passage_id in tqdm(passage_id_to_external_id, unit=" passages", desc="Setting external IDs"):
+            if by_task_id:
+                task = self.get_task(passage_id)
+                passage_id = task["passage"]["id"]
             passage = self.get_passage(passage_id)
             passage["external_id"] = external_id
             passage_out = self.update_passage(**passage)
@@ -27,6 +30,7 @@ class ExternalIdSetter(ServerAccessor):
     @staticmethod
     def add_arguments(argparser):
         argparser.add_argument("filename", help="file with lines of the form <EXTERNAL ID> <PASSAGE ID>")
+        argparser.add_argument("--by-task-id", action="store_true", help="expect task ID instead of passage ID")
         ServerAccessor.add_arguments(argparser)
 
 
